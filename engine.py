da9a164db825c96da2aeac23eb17f7860b1594ce
sshleifer <sshleifer@gmail.com>
2019-11-30 11:50:16 -0800

    RMSE backward

--- engine.py
+++ engine.py
@@ -16,7 +16,7 @@ class Trainer():
         predict = self.scaler.inverse_transform(output)
         real = torch.unsqueeze(real_val, dim=1)
         mae, mape, rmse = util.cheaper_metric(predict, real, null_val=0.0)
-        mae.backward()
+        rmse.backward()
         if self.clip is not None:
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip)
         self.optimizer.step()
